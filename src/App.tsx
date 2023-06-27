f372ca5a3ec158ea7727bb896a5382ecfcc24859
Sahil26299 <sahillokhande94@gmail.com>
2023-06-27 07:04:28 +0530

    Splash screen added, and site released on production

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import { useContext } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import './App.scss'
 import { ColorSchema } from './Utils/GlobalState'
 import { BrowserRouter } from "react-router-dom";
 import TopNavBar from './Components/TopNavBar/NavBar';
+import SplashScreen from './Components/SplashComponent/SplashScreen';
+
 function App() {
   const Colors = useContext(ColorSchema);
+  const [ShowApp, setShowApp] = useState<boolean>(false)
+  useEffect(() => {
+    setTimeout(() => {
+      setShowApp(true)
+    }, 2000);
+  }, [])
+
   return (
-    <div style={{backgroundColor:Colors.newVar.BGColor}} >
-      <BrowserRouter>
-        <TopNavBar />
-      </BrowserRouter>
+    <div style={{ backgroundColor: Colors.newVar.BGColor }} >
+      {!ShowApp ?
+        <SplashScreen /> :
+        <BrowserRouter>
+          <TopNavBar />
+        </BrowserRouter>}
     </div>
   )
 }
---
+++ src/Components/SplashComponent/SplashScreen.tsx
@@ -0,0 +1,11 @@
+import './SplashScreen.scss'; // Import CSS for styling
+
+const SplashScreen = () => {
+  return (
+    <div className="splash-screen">
+      <h1>Loading...</h1>
+    </div>
+  );
+};
+
+export default SplashScreen;
